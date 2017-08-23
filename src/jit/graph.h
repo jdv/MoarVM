af5bcee85f327078a13a73f78a0e67a2ea9e5c56
Stefan Seifert <nine@detonation.org>
2017-08-23 13:41:26 +0200

    Compile code for calling native functions.
    MVM_nativecall_build builds a JIT graph manually and compiles it. The
    result is attached to the MVMNativeCallBody. For now the new nativeinvoke_*
    ops (mirroring invoke_*) will enter that code. Ideally we will replace the
    whole frame containing the nativeinvoke by JIT compiled code.
    
    The nativeinvoke_o op just invokes the JITed code which will handle argument
    fetching on its own so we can avoid the overhead of copying arguments into an
    nqp::list.
    
    We need to unblock GC on the current thread after we return from the
    native function and before we box the return value. Otherwise if GC is
    triggered by the boxing function, we will hang as GC is blocked.
    
    You can now tell the JIT that the C function you want to call should take an
    argument from WORK[cur_op + whatever] which is the equivalent of all those
    GET_REG(cur_op, whatever) in interp.c.
    Same for return values. When in interp.c you'd write GET_REG(cur_op, 0) = ...;
    you tell the JIT: node->u.call.rv_mode = MVM_JIT_RV_DYNIDX;
    box_rv_node->u.call.rv_idx = 0;

--- src/jit/graph.h
+++ src/jit/graph.h
@@ -97,7 +97,11 @@ typedef enum {
     MVM_JIT_LITERAL_PTR,
     MVM_JIT_REG_STABLE,
     MVM_JIT_REG_OBJBODY,
+    MVM_JIT_REG_DYNIDX,
     MVM_JIT_DATA_LABEL,
+    MVM_JIT_SAVED_RV,
+    MVM_JIT_ARG_I64,
+    MVM_JIT_ARG_PTR,
 } MVMJitArgType;
 
 struct MVMJitCallArg {
@@ -123,7 +127,9 @@ typedef enum {
     /* dereference and store */
     MVM_JIT_RV_DEREF,
     /* store local at address */
-    MVM_JIT_RV_ADDR
+    MVM_JIT_RV_ADDR,
+    /* store in register relative to cur_op */
+    MVM_JIT_RV_DYNIDX,
 } MVMJitRVMode;
 
 
@@ -174,6 +180,7 @@ typedef enum {
     MVM_JIT_NODE_JUMPLIST,
     MVM_JIT_NODE_CONTROL,
     MVM_JIT_NODE_DATA,
+    MVM_JIT_NODE_SAVE_RV,
 } MVMJitNodeType;
 
 struct MVMJitNode {
